cc1002fa5c5b279990077cd7c089ada3a2a8a40e
4a50 <jpjones22@gmail.com>
2021-03-06 16:10:44 -0800

    Vibrations and sound implemented

---
+++ LiteBerryPiMobile/LiteBerryPiMobile.Android/Services/Audio.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using LiteBerryPiMobile.Droid.Services;
+using LiteBerryPiMobile.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Audio))]
+namespace LiteBerryPiMobile.Droid.Services
+{
+	public class Audio : IAudio
+	{
+		public Audio()
+    {
+
+    }
+
+		public bool PlayAudioFile(string fileName)
+		{
+			var player = new MediaPlayer();
+			var fd = Android.App.Application.Context.Assets.OpenFd(fileName);
+			player.Prepared += (s, e) =>
+			{
+				player.Start();
+			};
+			player.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
+			player.Prepare();
+			return true;
+		}
+	}
+}
---
+++ LiteBerryPiMobile/LiteBerryPiMobile/Services/IAudio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiteBerryPiMobile.Services
+{
+  public interface IAudio
+  {
+    bool PlayAudioFile(string filename);
+  }
+}
